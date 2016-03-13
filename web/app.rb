773d51b2ecdd4a7af5cdf07c635a13b25cc78141
d356 <d356@users.noreply.github.com>
2016-03-12 22:47:57 -0500

    updates

--- web/app.rb
+++ web/app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/base'
 require 'json'
 require 'octokit'
+require 'git'
 
 class App < Sinatra::Base
   ACCESS_TOKEN = ENV['GITHUB_TOKEN']
@@ -24,37 +25,62 @@ class App < Sinatra::Base
       if @payload["action"] == "opened"
         process_pull_request(@payload["pull_request"])
       end
+    when "push"
+      process_push(@payload)
     end
   end
 
   helpers do
+    def process_push(push)
+      branch_name = push['ref']
+      repo_name = push['repository']['full_name']
+      clone_url = "http://www.github.com/" + repo_name + ".git"
+      @@output = "Building push to " + branch_name + " at " + repo_name + "<br/>SHA: " + push['after'] + "<br/><br/>"
+      
+      `cd .. ; rm -rf yabause`
+      g = Git.clone clone_url, "../yabause"
+      g.checkout branch_name
+
+      @@output = @@output  + `cd ../script ; sh ci-hook.sh`
+      
+      @@output.gsub!(/\r?\n/, "<br/>")
+    end
+
     def process_pull_request(pull_request)
       @url = "http://example.com"
       @context = "GameCI"
       @pr1 = pull_request['base']['repo']['full_name']
-      @pr2 = pull_request['head']['sha']
+      @sha = pull_request['head']['sha']
+      @branch_name = pull_request['head']['ref']
 
-      @client.create_status(@pr1, @pr2, 'pending',
+      @client.create_status(@pr1, @sha, 'pending',
         {
           target_url: @url, 
           description: "Checking game screenshots...", 
           context: @context
         })
 
-      @@output = "Building " + @pr1 + " at " + @pr2 + "<br/><br/>"
+      @@output = "Building " + pull_request['head']['label'] + " for " + @pr1 + "<br/>" + "SHA: " + @sha + "<br/><br/>"
+
+      @repo_name = pull_request["head"]["repo"]["full_name"]
+      @clone_url = "http://www.github.com/" + @repo_name + ".git"
+
+      `cd .. ; rm -rf yabause`
+      g = Git.clone @clone_url, "../yabause"
+      g.checkout @branch_name
 
       #execute build
       @@output = @@output  + `cd ../script ; sh ci-hook.sh`
 
       if @@output.include? "FAIL"
-        @client.create_status(@pr1, @pr2, 'failure', 
+        @client.create_status(@pr1, @sha, 'failure', 
           {
             target_url: @url, 
             description: "Screenshots didn't match.", 
             context: @context
           })
       else
-        @client.create_status(@pr1, @pr2, 'success', 
+        @client.create_status(@pr1, @sha, 'success', 
           {
             target_url: @url, 
             description: "All game screenshots matched.", 
